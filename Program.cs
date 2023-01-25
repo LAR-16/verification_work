f069d1d3afb9de64afa1daf1525409626ce00159
Alexandra <lebedeva1618@yandex.ru>
2023-01-25 16:04:48 +0300

    print array

--- Program.cs
+++ Program.cs
@@ -3,4 +3,5 @@
 Console.Clear();
 Console.WriteLine("Введите данные через пробел: ");
 string[] arrayOfString = Console.ReadLine().Split(" ");
-
+Console.WriteLine("Первоначальный массив: ");
+Console.WriteLine($"[{string.Join(", ", arrayOfString)}]");
