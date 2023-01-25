06a10e71ef57cdc397a85eca088186e8fa671c90
Alexandra <lebedeva1618@yandex.ru>
2023-01-25 16:06:15 +0300

    find count= new array size

--- Program.cs
+++ Program.cs
@@ -5,3 +5,12 @@ Console.WriteLine("Введите данные через пробел: ");
 string[] arrayOfString = Console.ReadLine().Split(" ");
 Console.WriteLine("Первоначальный массив: ");
 Console.WriteLine($"[{string.Join(", ", arrayOfString)}]");
+int count = 0;
+for (int i = 0; i < arrayOfString.Length; i++)
+    {
+        int b = arrayOfString[i].Length;
+        if (b <= 3)
+        {
+            count ++;
+        }
+    }
